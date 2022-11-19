b1e35a4e7b3ce178419c9e09dfb4a3ccebad11f2
Alamito <alamirbobroski12@gmail.com>
2022-11-19 10:41:42 -0300

    feat - only one req on the function get

--- web-js/main.js
+++ web-js/main.js
@@ -2,39 +2,48 @@ const boxURL = document.querySelector('#box-URL');
 const shortURL = document.querySelector('#shortURL');
 const serverURL = "http://127.0.0.1:8080/api/v1/customer-wallets";
 
-submit.onclick = function () {
-    let normalURL = boxURL.value;
-    sendURL(serverURL, normalURL);
-    
-}
 
-const updateURL = {
-    used: "true",
-    url: "aquela piranha"
-}
+sendNormalURL = (status, url) => {
+    const updateURL = {
+        used: status,
+        url: url,
+    }
 
-sendNormalURL = () => {
     submit.onclick = () => {
-        let normalURL = boxURL.value;
-        updateURL.url = normalURL;
+        if (updateURL.used === 'false') {
+            let normalURL = boxURL.value;
+            updateURL.url = normalURL;
+        }
         axios.put(`${serverURL}/1`, updateURL)
     }
 }
 
+updateStatus = (status, url) => {
+    const updateURL = {
+        used: status,
+        url: url,
+    }
+
+    axios.put(`${serverURL}/2`, updateURL)
+}
 
 getShortURL = (serverURL) => {
     axios.get(serverURL)
         .then(response => {
             const data = response.data.customerWallets.data[1];
             if (data.used == 'false') {
-                console.log(data.url);
+                shortURL.value = data.url;
+                return data.url;
             }
+        })
+        .then((data) => {
+            updateStatus('true', data)
         })
 }
 
-// setInterval(() => {
-//     getShortURL(serverURL);
-// }, 1000);
+setInterval(() => {
+    getShortURL(serverURL);
+}, 1000);
 
-sendNormalURL(serverURL);
+sendNormalURL('false');
 
